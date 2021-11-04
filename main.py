1ae68fd80c05019ca4428c31d410cc1c428fe1be
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-11-04 00:01:55 -0500

    Added check for songs in genre playlists to exist in Tyler Radio

--- main.py
+++ main.py
@@ -28,25 +28,23 @@ os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8888'
 spotipy_username = secureData.variable("SPOTIPY_USERNAME")
 
 songYears = []
-totalTracks = -1
 index = 0
 
-def show_tracks(tracks, playlistName):
+def show_tracks(tracks, playlistName, total_tracks):
     global index
     returnTracks = []
 
     for i, item in enumerate(tracks['items']):
         track = item['track']
         index+=1
-        print(f"{index} of {totalTracks} in {playlistName}...")
+        print(f"{index} of {total_tracks} in {playlistName}...")
 
         if track['is_local'] == False:
             returnTracks.append(track['external_urls']['spotify'])
     
     return returnTracks
 
-if __name__ == '__main__':
-
+def extract():
     playlists = secureData.array("SPOTIPY_PLAYLISTS")
 
     try:
@@ -69,15 +67,39 @@ if __name__ == '__main__':
         playlist_name = playlist.split(',')[1]
         results = sp.playlist(playlist_id)
         tracks = results['tracks']
-        totalTracks = results['tracks']['total']
+        total_tracks = results['tracks']['total']
 
         playlist_tracks = []
 
         # go through each set of songs, 100 at a time (due to API limits)
+        global index
+        index = 0
+        playlist_tracks += (show_tracks(tracks, playlist_name, total_tracks))
         while tracks['next']:
-            playlist_tracks += (show_tracks(tracks, playlist_name))
             tracks = sp.next(tracks)
+            playlist_tracks += (show_tracks(tracks, playlist_name, total_tracks))
         
-        playlists_tracks.append(playlist_tracks)
+        playlists_tracks.append([playlist_name, playlist_tracks])
             
-    secureData.write("TEST", str(playlists_tracks))
+    return playlists_tracks
+
+def checkForAinB(a_index, b_index, tracks):
+    secureData.log(f"Checking that every track in {tracks[a_index][0]} is in {tracks[b_index][0]}", logName="LOG_SPOTIFY")
+    success = True
+    for track in tracks[a_index][1]:
+        if(track not in tracks[b_index][1]):
+            success = False
+            secureData.log(f"{track} not in {tracks[b_index][0]}", logName="LOG_SPOTIFY")
+    if(success):
+        secureData.log(f"Looks good!", logName="LOG_SPOTIFY")
+
+if __name__ == '__main__':
+    playlists_tracks = extract()
+    secureData.write("SPOTIPY_PLAYLIST_DATA", str(playlists_tracks))
+
+    # Caution- this code is necessarily fragile and assumes the data in the `SPOTIPY_PLAYLISTS` file
+    # matches the example file in README.md.
+
+    # 1. Check `Last 25 Added` and songs from each genre playlist is in `Tyler Radio`
+    for i, playlist in enumerate(playlists_tracks[1:]):
+        checkForAinB(i+1, 0, playlists_tracks)
